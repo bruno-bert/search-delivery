e758cb09386f7e5cd89866d38d760c87c6758bd3
Bruno de Paula <bruno.bert.jj@gmail.com>
2020-04-16 09:17:07 -0300

    added initialization script  to mongo docker

---
+++ adonis-backend/database/migrations-mongo/readme.md
@@ -0,0 +1,2 @@
+migrations-mongo: not using these migrations anymore, since there is a init.js script that initializes the collections and indexes in docker
+migrations-pg: not using postgres anymore
--- mongo/init.js
+++ mongo/init.js
@@ -1,3 +1,4 @@
+/** create user */
 db.createUser({
   user: "admin",
   pwd: "12345",
@@ -6,3 +7,19 @@ db.createUser({
     { role: "dbAdmin", db: "delivery" }
   ]
 });
+
+/** create the collections */
+db.createCollection("cities");
+db.createCollection("segments");
+db.createCollection("users");
+db.createCollection("tokens");
+db.createCollection("states");
+db.createCollection("raw");
+db.createCollection("shops");
+
+/** create indexes */
+db.segments.createIndex({ name: 1 }, { name: "name_index", unique: true });
+db.users.createIndex({ email: 1 }, { name: "email_index", unique: true });
+db.tokens.createIndex({ token: 1 }, { name: "token_index", unique: true });
+db.shops.createIndex({ location: "2dsphere" }, { name: "geo_index" });
+db.raw.createIndex({ place_id: 1 }, { name: "place_id_index", unique: true });
